4f60d85b490b829da6f1c6081a57be4a1c91c177
hoangyen0501 <151611109+hoangyen51@users.noreply.github.com>
2025-01-17 14:43:11 +0700

    Add files via upload

---
+++ 16_1_2025/salesmanagement/test/test_login.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from salesmanagement.ui.LoginMainWindowExt import LoginMainWindowExt
+
+app=QApplication([])
+mainwindow=QMainWindow()
+myui=LoginMainWindowExt()
+myui.setupUi(mainwindow)
+myui.showWindow()
+app.exec()
+
